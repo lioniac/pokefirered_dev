602c7efdb4f5f7e19e9ea69bc26a68871e58282a
lioniac <lioniac@protonmail.com>
2021-04-10 00:09:38 -0300

    [GenV] Obedience Level like modern games (Credit: Ketsuban)

--- src/battle_util.c
+++ src/battle_util.c
@@ -3179,12 +3179,20 @@ u8 IsMonDisobedient(void)
         if (!IsOtherTrainer(gBattleMons[gBattlerAttacker].otId, gBattleMons[gBattlerAttacker].otName) || FlagGet(FLAG_BADGE08_GET))
             return 0;
         obedienceLevel = 10;
+        if (FlagGet(FLAG_BADGE01_GET))
+            obedienceLevel = 20;
         if (FlagGet(FLAG_BADGE02_GET))
             obedienceLevel = 30;
+        if (FlagGet(FLAG_BADGE03_GET))
+            obedienceLevel = 40;
         if (FlagGet(FLAG_BADGE04_GET))
             obedienceLevel = 50;
+        if (FlagGet(FLAG_BADGE05_GET))
+            obedienceLevel = 60;
         if (FlagGet(FLAG_BADGE06_GET))
             obedienceLevel = 70;
+        if (FlagGet(FLAG_BADGE07_GET))
+            obedienceLevel = 80;
     }
     if (gBattleMons[gBattlerAttacker].level <= obedienceLevel)
         return 0;
