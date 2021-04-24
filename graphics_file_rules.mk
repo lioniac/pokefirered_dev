28b6a673c2d0cf07626ae237ff500c60e29c381d
lioniac <lioniac@protonmail.com>
2021-04-23 21:49:09 -0300

    [GenVI] Fairy Type, Type Effectiveness Chart Updated (All Credit to hjk321 - Thanks man!)

--- graphics_file_rules.mk
+++ graphics_file_rules.mk
@@ -25,7 +25,7 @@ CREDITSGFXDIR := graphics/credits
 ITEMMENUGFXDIR := graphics/item_menu
 INTROGFXDIR := graphics/intro
 
-types := normal fight flying poison ground rock bug ghost steel mystery fire water grass electric psychic ice dragon dark
+types := normal fight flying poison ground rock bug ghost steel mystery fire water grass electric psychic ice dragon dark fairy
 contest_types := cool beauty cute smart tough
 
 CASTFORMGFXDIR := graphics/pokemon/castform
